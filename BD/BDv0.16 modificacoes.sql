18946699720f27925750faa0612f895db52505d3
Gustavo Dauer <GustavoDauer@users.noreply.github.com>
2026-02-03 22:31:21 -0300

    Modify Sped and Pessoa tables with new columns and constraints

---
+++ BD/BDv0.16 modificacoes.sql
@@ -0,0 +1,18 @@
+ALTER TABLE `scc`.`Sped` 
+ADD COLUMN `assunto` VARCHAR(250) NULL AFTER `tipo`,
+ADD COLUMN `arquivo` BLOB NULL AFTER `assunto`;
+
+ALTER TABLE `scc`.`Pessoa` 
+ADD COLUMN `telefone` VARCHAR(14) NULL AFTER `dataExpiracao`;
+
+ALTER TABLE `scc`.`Sped` 
+DROP COLUMN `responsavel`,
+ADD COLUMN `Pessoa_idPessoa` INT NULL AFTER `arquivo`,
+ADD INDEX `fk_Pessoa_idPessoa_idx` (`Pessoa_idPessoa` ASC);
+;
+ALTER TABLE `scc`.`Sped` 
+ADD CONSTRAINT `fk_Pessoa_idPessoa`
+  FOREIGN KEY (`Pessoa_idPessoa`)
+  REFERENCES `scc`.`Pessoa` (`idPessoa`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
